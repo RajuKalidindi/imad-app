e47ab9cb01fd8f98d380b4950c42d7da76d7263b
RajuKalidindi <30723201+RajuKalidindi@users.noreply.github.com>
2018-02-28 00:20:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ element.innerHTML = "new value";
 var move = document.getElementById("im");
 var marginLeft = 0;
 function moveRight(){
-    marginLeft =marginLeft + 10;
+    marginLeft =marginLeft + 5;
 im.style.marginLeft = marginLeft + 'px';
     
 }
