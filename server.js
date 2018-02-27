0d7d482931e52f4710d17a4a77a3478e988b5f19
RajuKalidindi <30723201+RajuKalidindi@users.noreply.github.com>
2018-02-27 16:24:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-1', function (req,res){
+  res.send("Aricle one req is here");  
+});
+
+
+app.get('/article-2', function (req,res){
+  res.send("Aricle two req is here");  
+});
+
+
+app.get('/article-3', function (req,res){
+  res.send("Aricle three req is here");  
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
