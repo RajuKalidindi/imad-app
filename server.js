757531a1a5299688f6367a0933494426750cc607
RajuKalidindi <30723201+RajuKalidindi@users.noreply.github.com>
2018-02-27 17:42:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,16 +93,6 @@ app.get('/:articleName', function (req,res){
 var articleName=req.params.articleName;
 });
 
-
-app.get('/article-2', function (req,res){
- res.sendFile(path.join(__dirname, 'ui', 'article-2.html'));  
-});
-
-
-app.get('/article-3', function (req,res){
- res.sendFile(path.join(__dirname, 'ui', 'article-3.html')); 
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
