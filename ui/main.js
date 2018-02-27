385956f63f56021b3545de132e8e85876def5931
RajuKalidindi <30723201+RajuKalidindi@users.noreply.github.com>
2018-02-28 00:17:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,12 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = "new value";
 var move = document.getElementById("im");
+function moveRight(){
+    moveLeft =moveLeft + 1;
+im.style.marginLeft = marginLeft + 'px';
+    
+}
 im.onclick =function(){
-  im.style.marginLeft = '300px';
+    var interval =setInterval(moveRight , 50);
+  
 };
