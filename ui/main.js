03ee633827bd5e8badfb6d8bff24a6e857ae6224
RajuKalidindi <30723201+RajuKalidindi@users.noreply.github.com>
2018-03-06 23:03:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,11 +22,25 @@ var nameInput = document.getElementById("name");
 var name = nameInput.value;
 var submit = document.getElementById("submit_btn");
 submit.onclick = function(){
-    var names = ['name1','name2','name3'];
+     //create request
+    var request = new XMLHttpRequest();
+    //capture response and store it in a var
+    request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE){
+          if(request.status === 200){
+              var names = request.responseText;
+              names = JSON.parse(names);
     var lists = '';
     for(var i=0;i<names.length;i++){
         lists += '<li>'+ names[i] +'</li>';
     }
     var ul = document.getElementById("nameList");
-    ul.innerHTML = lists;
+    ul.innerHTML = lists; 
+          }
+      }  
+    };
+    //make request
+    request.open('GET', 'http://krkraju997.imad.hasura-app.io/submit-name?name='+ name, true);
+    request.send(null);
+   
 };
