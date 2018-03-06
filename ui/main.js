a5cab2024a7c1fad62220c84be8f69c10d1abc2e
RajuKalidindi <30723201+RajuKalidindi@users.noreply.github.com>
2018-03-06 22:12:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,8 @@
-console.log('Loaded!');
-var element = document.getElementById("main-text");
-element.innerHTML = "new value";
-var move = document.getElementById("im");
-var marginLeft = 0;
-function moveRight(){
-    marginLeft =marginLeft + 5;
-im.style.marginLeft = marginLeft + 'px';
-    
-}
-im.onclick =function(){
-    var interval =setInterval(moveRight , 50);
-  
+//counter code
+var button = document.getElementById("counter");
+var counter = 0;
+button.onclick = function() {
+    counter = counter + 1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
 };
