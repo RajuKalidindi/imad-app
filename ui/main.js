c0182ec8ef67d692f58f6b6384873d319c6506e1
RajuKalidindi <30723201+RajuKalidindi@users.noreply.github.com>
2018-03-06 22:44:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,7 +25,7 @@ submit.onclick = function(){
     var names = ['name1','name2','name3'];
     var lists = '';
     for(var i=0;i<names.length;i++){
-        lists += '<li>'+names[i]+'</li';
+        lists += '<li>'+ names[i] +'</li>';
     }
     var ul = document.getElementById("nameList");
     ul.innerHTML = lists;
