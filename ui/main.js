fa09af87cec919d7832c1ec82e4d12d6f18a9503
RajuKalidindi <30723201+RajuKalidindi@users.noreply.github.com>
2018-03-06 23:06:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,8 +18,7 @@ button.onclick = function() {
     request.send(null);
 };
 //submit name
-var nameInput = document.getElementById("name");
-var name = nameInput.value;
+
 var submit = document.getElementById("submit_btn");
 submit.onclick = function(){
      //create request
@@ -40,6 +39,8 @@ submit.onclick = function(){
       }  
     };
     //make request
+    var nameInput = document.getElementById("name");
+    var name = nameInput.value;
     request.open('GET', 'http://krkraju997.imad.hasura-app.io/submit-name?name='+ name, true);
     request.send(null);
    
