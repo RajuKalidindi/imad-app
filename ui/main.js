1394b885d08997f5c602f1b7d58660cddb3798f8
RajuKalidindi <30723201+RajuKalidindi@users.noreply.github.com>
2018-03-06 22:41:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,9 @@
 //counter code
 var button = document.getElementById("counter");
 button.onclick = function() {
-    
+    //create request
     var request = new XMLHttpRequest();
-    
+    //capture response and store it in a var
     request.onreadystatechange = function(){
       if(request.readyState === XMLHttpRequest.DONE){
           if(request.status === 200){
@@ -13,6 +13,20 @@ button.onclick = function() {
           }
       }  
     };
+    //make request
     request.open('GET', 'http://krkraju997.imad.hasura-app.io/counter', true);
     request.send(null);
+};
+//submit name
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
+var submit = document.getElementById("submit_btn");
+submit.onclick = function(){
+    var names = ['name1','name2','name3'];
+    var lists = '';
+    for(var i=0;i<names.length;i++){
+        lists += '<li>'+names[i]+'</li';
+    }
+    var ul = document.getElementById("nameList");
+    ul.innerHTML = list;
 };
